04e635d7f262124ff8ecd8d42535d693d97fd288
Samuel Holmes <samuelholmes30@gmail.com>
2020-11-01 08:49:35 -0600

    updating code

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -13,7 +13,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  var questions = [
+  //protect questions variable from changes
+  final _questions = const [
     {
       'questionText': ('What\'s your favorite color?'),
       'answers': ["Black", "red", "green", "white", "blue"]
@@ -31,12 +32,10 @@ class _MyAppState extends State<MyApp> {
   //will be our callback function; the function (address) we are passing around is also known as a
   //callback function bc the receiving widget calls it in the future
   void _answerQuestion(int num) {
+    //bool type; this expression yields true if condition is met
+
     setState(() {
-      if (_questionIndex + 1 < questions.length) {
-        _questionIndex = num + 1;
-      } else {
-        _questionIndex = 0;
-      }
+      _questionIndex = num + 1;
     });
   }
 
@@ -47,18 +46,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questionText: questions[_questionIndex]["questionText"]),
-            //spread operator pulls items in a list out of the list
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(
-                  selectHandler: () => _answerQuestion(_questionIndex),
-                  text: answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
+              )
+            : Result(),
       ),
     );
   }
